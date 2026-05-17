4dbd6a982239483cc008db4c09e9f85e7fc75a7f
jomoetnt <jomoetnt@gmail.com>
2026-05-17 18:08:37 -0400

    preview list combined

--- build_hp.py
+++ build_hp.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import json
 
 subpagePaths = {'jeffHome.html': 'index.html', 'articles/jeffArticles.html': 'articles/index.html', 'about/jeffAbout.html': 'about/index.html'}
 tokenPaths = {r'{jeffHeader}': 'header.html', r'{jeffFooter}': 'footer.html', r'{jeffArticleList}': 'articles/article_list.html'}
@@ -12,38 +13,57 @@ for articleName in os.listdir('articles'):
         articleOutputPath = 'articles/' + articleName + '/index.html'
         articlePaths[articleInputPath] = articleOutputPath
 
-# make inputPath-articleText dictionary and date-inputPath dictionary
-articleTexts = {}
-articleDates = {}
+class jeffArticle:
+    def __init__(self, title, teaser, date, topic, colour, thumbnail, bodyText, outputPath):
+        self.title = title
+        self.teaser = teaser
+        self.date = date
+        self.topic = topic
+        self.colour = colour
+        self.thumbnail = thumbnail
+        self.bodyText = bodyText
+        self.outputPath = outputPath
+    
+    def replaceTokens(self, inputText):        
+        return inputText.replace(r'{title}', self.title).replace(r'{teaser}', self.teaser).replace(r'{date}', self.date.strftime('%B %d, %Y')).replace(r'{topic}', self.topic).replace(r'{colour}', self.colour).replace(r'{thumbnail}', self.thumbnail).replace(r'{link}', self.outputPath.replace('index.html', ''))
+
+# make outputPath-article dictionary
+jeffArticles = {}
 for articlePath in list(articlePaths.keys()):
-    # extract date from first line and remove it from article text
-    dateLine = ''
+    # read article text
     articleText = ''
     with open(articlePath, 'r', encoding='utf-8') as articleFile:
-        lines = articleFile.readlines()
-        dateLine = lines[0].replace('\n', '')
-        articleText = ''.join(lines[1:])
-    articleTexts[articlePath] = articleText
+        articleText = articleFile.read()
 
-    articleDate = datetime.date.fromisoformat(dateLine)
-    articleDates[articleDate.toordinal()] = articlePath
+    # read article details
+    articlePreviewPath = articlePath.replace('article.html', 'article_details.json')
+    articleMetadata = {}
+    with open(articlePreviewPath, 'r', encoding='utf-8') as articleFile:
+        articleDetails = json.load(articleFile)
+
+        articleMetadata = articleDetails
+
+        articleDate = datetime.date.fromisoformat(articleDetails['date'])
+        articleMetadata['date'] = articleDate.toordinal()
+
+    # add article to dictionary
+    jeffArticles[articlePaths[articlePath]] = jeffArticle(articleMetadata['title'], articleMetadata['date'], articleMetadata['thumbnail'], articleText)
 
 # sort articlePaths by date
-sortedArticlePaths = []
-for articleDate in sorted(list(articleDates.keys()), reverse=True):
-    sortedArticlePaths.append(articleDates[articleDate])
-
-# read article previews
-articlePreviewList = ''
-for articlePath in sortedArticlePaths[:9]:
-    articlePreviewPath = articlePath.replace('article.html', 'article_preview.html')
-    with open(articlePreviewPath, 'r', encoding='utf-8') as articlePreviewFile:
-        articlePreviewText = articlePreviewFile.read()
-        articlePreviewList += articlePreviewText
+sortedArticles = sorted(list(jeffArticles.values()), key=lambda jeffArticle: jeffArticle.date, reverse=True)
 
 # write article list
-with open('articles/article_list.html', 'w', encoding='utf-8') as jeffArticleList:
-    jeffArticleList.write(articlePreviewList)
+with open('articles/article_list_item.html', 'r', encoding='utf-8') as jeffArticleItem:
+    # make replacements to preview
+    jeffArticlePreviewTemplate = jeffArticleItem.read()
+    jeffArticlePreviews = []
+    for sortedArticle in sortedArticles[:9]:
+        jeffArticlePreview = sortedArticle.replaceTokens(jeffArticlePreviewTemplate)
+        jeffArticlePreviews.append(jeffArticlePreview)
+    
+    # combine previews
+    with open('articles/article_list.html', 'w', encoding='utf-8') as jeffArticleList:
+        jeffArticleList.write(''.join(jeffArticlePreviews))
 
 # read token files
 tokenTexts = {}
